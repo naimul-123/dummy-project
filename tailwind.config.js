730e9ad8f1da288f9c087b0f5246a11db9e6747f
Naimul-Islam <naimuliu@gmail.com>
2024-02-24 02:04:34 +0600

    First page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
     content: ["./**/*.{html,js}"],
     theme: {
+        colors: {
+            'feldgrau': '#436850',
+            'laurelGreen':'#ADBC9F',
+            'raisinBlack':'#252525',
+            'white': '#FFFFFF',
+            'ghostWhite': '#FAFCFE',
+            'antiFlashWhite': '#F3F3F3',
+          },
+        fontFamily: {
+            inter: ['Inter', 'sans-serif'],
+          },
       extend: {},
     },
-    plugins: [],
+    
+    
+    plugins: [require("daisyui")],
   }
